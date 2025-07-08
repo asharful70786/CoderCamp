42ac8ab595cbb657c3688cc85fda8359ec0454eb
Ashraful <ashrafulmomin7076@gmail.com>
2025-07-09 01:59:17 +0530

    verser ser probelm

--- server/app.js
+++ server/app.js
@@ -8,23 +8,16 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-
 app.use(cors({
   origin: "https://codercamp.ashraful.in", 
- 
 }));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
-})
-
+});
 
 app.use("/api/contact", mailRoutes);
 app.use("/api/subscribe", subscribeRoutes);
 
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
