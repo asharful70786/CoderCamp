ff79de1d113ea3a67f058fd40f4ca37086eb090a
Ashraful <ashrafulmomin7076@gmail.com>
2025-07-09 02:08:25 +0530

    deploy for vercel for resolved

--- server/app.js
+++ server/app.js
@@ -19,5 +19,7 @@ app.get("/", (req, res) => {
 app.use("/api/contact", mailRoutes);
 app.use("/api/subscribe", subscribeRoutes);
 
-
-export default app;
+console.log(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
